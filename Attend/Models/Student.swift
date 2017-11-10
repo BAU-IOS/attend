45482541b3bd21d7c674ecc63e0a3343dfff7590
kahyalar <furkan.kahyalar@gmail.com>
2017-11-10 18:11:25 +0300

    Student model refactored

--- Attend/Models/Student.swift
+++ Attend/Models/Student.swift
@@ -10,16 +10,21 @@ import Foundation
 
 class Student
 {
-    let fullName: String
+    var fullName: String
+    let firstName: String
+    let lastName: String
     let studentNumber: String
     let department: String
     let classNo: Int
     
-    init(fullName: String, studentNumber: String, department: String, classNo: Int)
+    init(firstName: String, lastName: String, studentNumber: String, department: String, classNo: Int)
     {
-        self.fullName = fullName
+        self.firstName = firstName
+        self.lastName = lastName
         self.studentNumber = studentNumber
         self.department = department
         self.classNo = classNo
+        
+        fullName = firstName + " " + lastName
     }
 }
