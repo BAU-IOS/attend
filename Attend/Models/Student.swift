6963604cb5b0136a32744dd306ec22fb8af68bfc
kahyalar <furkan.kahyalar@gmail.com>
2017-11-10 17:14:39 +0300

    Student model added

---
+++ Attend/Models/Student.swift
@@ -0,0 +1,25 @@
+//
+//  Student.swift
+//  Attend
+//
+//  Created by Furkan Kahyalar on 10/11/2017.
+//  Copyright © 2017 suat.karakusoglu. All rights reserved.
+//
+
+import Foundation
+
+class Student
+{
+    let fullName: String
+    let studentNumber: String
+    let department: String
+    let classNo: Int
+    
+    init(fullName: String, studentNumber: String, department: String, classNo: Int)
+    {
+        self.fullName = fullName
+        self.studentNumber = studentNumber
+        self.department = deparment
+        self.classNo = classNo
+    }
+}
